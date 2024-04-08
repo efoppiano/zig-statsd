e9f3e160ace638b67a1fc4f269e6aae6111a1bb8
efoppiano <efoppiano@fi.uba.ar>
2024-04-08 03:06:41 -0300

    Add support for no prefix

--- src/main.zig
+++ src/main.zig
@@ -7,7 +7,7 @@ pub const StatsDConfig = struct { host: []const u8 = "127.0.0.1", port: u16 = 81
 
 pub const StatsDClient = struct {
     stream: net.Stream,
-    prefix: []const u8,
+    prefix: ?[]const u8,
     allocator: Allocator,
 
     const Self = @This();
@@ -15,10 +15,16 @@ pub const StatsDClient = struct {
     pub fn init(config: StatsDConfig) !Self {
         const stream = try Self.create_udp_stream(config.host, config.port);
         errdefer stream.close();
-        var prefix = try config.allocator.alloc(u8, config.prefix.len);
-        std.mem.copy(u8, prefix, config.prefix);
 
-        return Self{ .stream = stream, .prefix = prefix, .allocator = config.allocator };
+        var ret = Self{ .stream = stream, .prefix = null, .allocator = config.allocator };
+
+        if (config.prefix) |pr| {
+            const prefix = try config.allocator.alloc(u8, pr.len);
+            std.mem.copy(u8, prefix, pr);
+            ret.prefix = prefix;
+        }
+
+        return ret;
     }
 
     pub fn deinit(self: Self) void {
@@ -33,6 +39,28 @@ pub const StatsDClient = struct {
         return net.Stream{ .handle = fd };
     }
 
+    fn alloc_metric(self: Self, slices: []const[]const u8) ![]u8 {
+        if (self.prefix) |prefix| {
+            var buf_size: usize = prefix.len + 1;
+            for (slices) |slice| {
+                buf_size += slice.len;
+            }
+            const buf = try self.allocator.alloc(u8, buf_size);
+            errdefer self.allocator.free(buf);
+
+            std.mem.copy(u8, buf, prefix);
+            std.mem.copy(u8, buf, ".");
+            var buf_index: usize = prefix.len + 1;
+            for (slices) |slice| {
+                std.mem.copy(u8, buf[buf_index..], slice);
+                buf_index += slice.len;
+            }
+            return buf;
+        } else {
+            return std.mem.concat(self.allocator, u8, slices);
+        }
+    }
+
     fn number_to_str(self: Self, a: anytype) ![]u8 {
         return std.fmt.allocPrint(self.allocator, "{d}", .{a});
     }
@@ -41,7 +69,7 @@ pub const StatsDClient = struct {
         const value_str = try self.number_to_str(value);
         defer self.allocator.free(value_str);
 
-        const metric = try std.mem.concat(self.allocator, u8, &[_][]const u8{ self.prefix, ".", metric_name, ":", value_str, "|c" });
+        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|c" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
@@ -59,7 +87,7 @@ pub const StatsDClient = struct {
         const value_str = try self.number_to_str(value);
         defer self.allocator.free(value_str);
 
-        const metric = try std.mem.concat(self.allocator, u8, &[_][]const u8{ self.prefix, ".", metric_name, ":", value_str, "|ms" });
+        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|ms" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
@@ -69,7 +97,7 @@ pub const StatsDClient = struct {
         const value_str = try self.number_to_str(value);
         defer self.allocator.free(value_str);
 
-        const metric = try std.mem.concat(self.allocator, u8, &[_][]const u8{ self.prefix, ".", metric_name, ":", value_str, "|g" });
+        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|g" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
