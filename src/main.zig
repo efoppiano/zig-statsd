2ac674775d3404e1130aa3f05185322e2736fd19
efoppiano <efoppiano@fi.uba.ar>
2024-04-08 14:36:22 -0300

    Add documentation

--- src/main.zig
+++ src/main.zig
@@ -3,13 +3,22 @@ const os = std.os;
 const net = std.net;
 const Allocator = std.mem.Allocator;
 
-pub const StatsDConfig = struct { host: []const u8 = "127.0.0.1", port: u16 = 8125, allocator: Allocator = std.heap.page_allocator, prefix: ?[]const u8 = null };
+pub const StatsDConfig = struct {
+    host: []const u8 = "127.0.0.1",
+    port: u16 = 8125,
+    // It is strongly recommended that you use another allocator, because this one makes a syscall for every alloc,
+    // and the StatsDClient uses it to concatenate the metric string that will be sent
+    allocator: Allocator = std.heap.page_allocator,
+    prefix: ?[]const u8 = null,
+};
 
+/// Simple pseudo random number generator to avoid depending on the std implementation, which caused some problems
+/// Anyway, this use case does not require an extremely good generator
 const Rng = struct {
     prev: u128,
 
     pub fn init(seed: u64) Rng {
-        return .{.prev = seed};
+        return .{ .prev = seed };
     }
 
     pub fn next(self: *Rng) u32 {
@@ -21,7 +30,7 @@ const Rng = struct {
     pub fn next_f64(self: *Rng) f64 {
         const n = self.next();
         const n_f32: f32 = @floatFromInt(n);
-        return @as(f64, n_f32/(1 << 32));
+        return @as(f64, n_f32 / (1 << 32));
     }
 };
 
@@ -34,6 +43,8 @@ pub const StatsDClient = struct {
 
     const Self = @This();
 
+    /// Creates a new client that sends metrics over UDP
+    /// The metrics will be sent one by one
     pub fn init(config: StatsDConfig) !Self {
         var ret: Self = undefined;
 
@@ -56,6 +67,7 @@ pub const StatsDClient = struct {
         return ret;
     }
 
+    /// Closes the stream and frees the allocated memory
     pub fn deinit(self: Self) void {
         if (self.prefix) |prefix| {
             self.allocator.free(prefix);
@@ -77,7 +89,10 @@ pub const StatsDClient = struct {
         return net.Stream{ .handle = fd };
     }
 
-    fn alloc_metric(self: Self, slices: []const[]const u8) ![]u8 {
+    /// Concatenates the strings to form the metric to send
+    /// If self.prefix is set, it will add it to the start of the metric
+    /// Caller owns the memory
+    fn alloc_metric(self: Self, slices: []const []const u8) ![]u8 {
         if (self.prefix) |prefix| {
             var buf_size: usize = prefix.len + 1;
             for (slices) |slice| {
@@ -103,10 +118,12 @@ pub const StatsDClient = struct {
         return std.fmt.allocPrint(self.allocator, "{d}", .{a});
     }
 
+    /// Modify a counter by <value>
     pub fn count(self: Self, metric_name: []const u8, value: f64) !void {
         return self.send_sampled_count(metric_name, value, null);
     }
 
+    /// Modify a counter by <value> only <rate>*100% of the time
     pub fn sampled_count(self: *Self, metric_name: []const u8, value: f64, rate: f64) !void {
         if (self.should_sample()) {
             return self.send_sampled_count(metric_name, value, rate);
@@ -120,9 +137,9 @@ pub const StatsDClient = struct {
         if (rate) |r| {
             const rate_str = try self.number_to_str(r);
             defer self.allocator.free(rate_str);
-            metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|c|@", rate_str });
+            metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":", value_str, "|c|@", rate_str });
         } else {
-            metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|c" });
+            metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":", value_str, "|c" });
         }
 
         defer self.allocator.free(metric);
@@ -130,24 +147,28 @@ pub const StatsDClient = struct {
         try self.stream.writeAll(metric);
     }
 
+    /// Increment a metric by 1
     pub fn incr(self: Self, metric_name: []const u8) !void {
         return self.count(metric_name, 1.0);
     }
 
+    /// Decrement a metric by 1
     pub fn decr(self: Self, metric_name: []const u8) !void {
         return self.count(metric_name, -1.0);
     }
 
+    /// Send a timer with <value>, in ms
     pub fn timer(self: Self, metric_name: []const u8, value: f64) !void {
         const value_str = try self.number_to_str(value);
         defer self.allocator.free(value_str);
 
-        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|ms" });
+        const metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":", value_str, "|ms" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
     }
 
+    /// Send a timer with <value>, in ms, only <rate>*100% of the time
     pub fn sampled_timer(self: *Self, metric_name: []const u8, value: f64, rate: f64) !bool {
         if (self.should_sample(rate)) {
             return self.send_sampled_timer(metric_name, value, rate);
@@ -161,40 +182,43 @@ pub const StatsDClient = struct {
         if (rate) |r| {
             const rate_str = try self.number_to_str(r);
             defer self.allocator.free(rate_str);
-            metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|ms|@", rate_str });
+            metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":", value_str, "|ms|@", rate_str });
         } else {
-            metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|ms" });
+            metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":", value_str, "|ms" });
         }
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
     }
 
+    /// Set a gauge to <value>
     pub fn gauge(self: Self, metric_name: []const u8, value: f64) !void {
         const value_str = try self.number_to_str(value);
         defer self.allocator.free(value_str);
 
-        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":", value_str, "|g" });
+        const metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":", value_str, "|g" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
     }
 
+    /// Increment a gauge by <value>
     pub fn gauge_incr(self: Self, metric_name: []const u8, amount: f64) !void {
         const amount_str = try self.number_to_str(amount);
         defer self.allocator.free(amount_str);
 
-        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":+", amount_str, "|g" });
+        const metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":+", amount_str, "|g" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
     }
 
+    /// Decrement a gauge by <value>
     pub fn gauge_decr(self: Self, metric_name: []const u8, amount: f64) !void {
         const amount_str = try self.number_to_str(amount);
         defer self.allocator.free(amount_str);
 
-        const metric = try self.alloc_metric(&[_][]const u8{metric_name, ":-", amount_str, "|g" });
+        const metric = try self.alloc_metric(&[_][]const u8{ metric_name, ":-", amount_str, "|g" });
         defer self.allocator.free(metric);
 
         try self.stream.writeAll(metric);
